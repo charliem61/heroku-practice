7f6bc7dbdaa940c19ba95a5e87f4117187c16038
Mason Walker <masonwalker30@gmail.com>
2022-01-22 15:00:11 -0800

    all files added/ work in diag

---
+++ Develop/routes/diagnostics.js
@@ -0,0 +1,21 @@
+const diagnostics = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving diagnostic information
+diagnostics.get('/', (req, res) => {
+
+    readFromFile('./db/diagnostics.json').then((data) => res.json(JSON.parse(data)))
+
+
+  // TODO: Logic for sending all the content of db/diagnostics.json
+  diagnostics.post('/', (req, res) => {})
+
+});
+
+// POST Route for a error logging
+diagnostics.post('/', (req, res) => {
+  // TODO: Logic for appending data to the db/diagnostics.json file
+});
+
+module.exports = diagnostics;
